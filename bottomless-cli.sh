a80b6ea6957d57c2f1579ec49e97981fcca0564f
dyu <david.yu.ftw@gmail.com>
2025-03-27 12:05:09 +0800

    cli script

---
+++ bottomless-cli.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#set -e
+
+CURRENT_DIR=$PWD
+# locate
+if [ -z "$BASH_SOURCE" ]; then
+    SCRIPT_DIR=`dirname "$(readlink -f $0)"`
+elif [ -e '/bin/zsh' ]; then
+    F=`/bin/zsh -c "print -lr -- $BASH_SOURCE(:A)"`
+    SCRIPT_DIR=`dirname $F`
+elif [ -e '/usr/bin/realpath' ]; then
+    F=`/usr/bin/realpath $BASH_SOURCE`
+    SCRIPT_DIR=`dirname $F`
+else
+    F=$BASH_SOURCE
+    while [ -h "$F" ]; do F="$(readlink $F)"; done
+    SCRIPT_DIR=`dirname $F`
+fi
+
+cd $SCRIPT_DIR
+
+build_cli() {
+  cd bottomless-cli
+  cargo build --release
+  cd - > /dev/null
+}
+
+REL_BIN='target/release/bottomless-cli'
+[ -e "$REL_BIN" ] || build_cli
+
+. .env
+./$REL_BIN $@
